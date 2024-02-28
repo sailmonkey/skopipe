045b34e117a433a5c1eacf34e36f5cdf4ad4f341
sailmonkey <jcastro@jcastro--macbookpro18.lan>
2024-02-28 12:03:31 -0500

    Committer: sailmonkey <jcastro@jcastro--macbookpro18.lan>  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   kafka-yfinance.py

--- kafka-yfinance.py
+++ kafka-yfinance.py
@@ -3,6 +3,7 @@ import yfinance as yahooFinance    # Yahoo finance API
 import time 
 import requests
 import json
+import socket
 
 # Kafka producer configuration
 headers = {
@@ -11,37 +12,37 @@ headers = {
     'Authorization': 'Bear <token>'
 }
 
-conf = {
-    'bootstrap.servers': '10.234.112.205:9092',
-    'client.id': 'python-price-producer',
-}
 
 # Create a producer instance
+conf = {'bootstrap.servers': '10.234.112.205:9092',
+        'client.id': socket.gethostname()}
 producer = Producer(conf)
+print('Kafka Producer has been initiated...')
 
 # Kafka topic to send the stock price data. Created manually using the kafka-topics.sh script
 topic = 'yfinance'
 
 # Ticker symbol for stock
-symbol = 'PSTG'
+tksymbol = 'PSTG'
 
 def fetch_and_send_stock_price():
  while True:
    try:
-     url = 'https://query2.finance.yahoo.com/v8/finance/chart/btc-usd'
+     url = 'https://query2.finance.yahoo.com/v8/finance/chart/'+tksymbol
      response = requests.get(url, headers=headers)
      data = json.loads(response.text)
      price = data["chart"]["result"][0]["meta"]["regularMarketPrice"]
+     print(tksymbol, ':', price)
      #Function to fetch stock price and send to Kafka
      # Produce the stock price to the Kafka topic
-     producer.produce(topic, key=symbol, value=str(price))
+     producer.produce(topic, key=str(tksymbol), value=str(price))
      producer.flush()
-     print(f"Sent {symbol} price to Kafka: {price}")
+     print(f"Sent {tksymbol} price to Kafka: {price}")
    except Exception as e:
     print(f"Error fetching/sending stock price: {e}")
 
 # Sleep for a specified interval (e.g., 5 seconds) before fetching the next price
-   time.sleep(120)
+   time.sleep(60)
 
 # Start sending stock price data
 fetch_and_send_stock_price()
